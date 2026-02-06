d52a74eea57b6dee44a3730dfb25b79a0cce839b
RedKrane86 <redkrane86@gmail.com>
2026-02-07 01:29:21 +0500

    Optimize

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-num_list_1 = [1, 2, 3, 4]
-num_list_2 = [10, 20, 30]
+from itertools import zip_longest
 
 def create_sum_list(list_1: list[int], list_2: list[int]) -> list[int]:
     """
     - Принимает списки с целочисленными
-    - Определяет минимальную длину списков
     - Подсчитывает сумму соответствующих элементов и кладет в новый список
+    - Если списки разной длинны, то недостающие цифры будут ровняться '0'
     - Возвращает новый список
     """
-    min_len = min(len(list_1), len(list_2))
-    result = []
-    for num in range(min_len):
-        result.append(num_list_1[num] + num_list_2[num])
-
+    result = [sum(nums) for nums in zip_longest(list_1, list_2, fillvalue=0)]
     return result
+
+num_list_1 = [1, 2, 3, 4, 5]
+num_list_2 = [10, 20, 30]
 
 print(create_sum_list(num_list_1, num_list_2))
